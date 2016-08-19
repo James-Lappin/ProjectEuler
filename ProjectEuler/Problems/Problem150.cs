76b5dfaea96c70b4ff4391b1802f6f6491812214
James Lappin <lappintheyoung@gmail.com>
2016-08-19 17:57:05 +0100

    Problem 150 solution. Should investigate other list types

--- ProjectEuler/Problems/Problem150.cs
+++ ProjectEuler/Problems/Problem150.cs
@@ -1,4 +1,6 @@
 ﻿using ProjectEuler.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ProjectEuler.Problems
 {
@@ -6,12 +8,65 @@ namespace ProjectEuler.Problems
     {
         public string Solve()
         {
-            throw new System.NotImplementedException();
+            const int amountOfSums = 5;
+            var jaggedArray = new int[6][];
+            jaggedArray[0] = new[] { 15 };
+            jaggedArray[1] = new[] { -14, -7 };
+            jaggedArray[2] = new[] { 20, -13, -5 };
+            jaggedArray[3] = new[] { -3, 8, 23, -26 };
+            jaggedArray[4] = new[] { 1, -4, -5, -18, 5 };
+            jaggedArray[5] = new[] { -16, 31, 2, 9, 28, 3 };
+
+            return GetSmallestSubtriangleSum(jaggedArray, amountOfSums).ToString();
         }
 
-        public static int[] GetSmallestSubtriangleSum(int[][] jaggedArray, int amountOfSums)
+        public int[] GetSmallestSubtriangleSum(int[][] jaggedArray, int amountOfSums)
         {
-            throw new System.NotImplementedException();
+            var sums = new List<int>();
+            for (var i = 0; i < jaggedArray.Length; i++)
+            {
+                for (var j = 0; j < jaggedArray[i].Length; j++)
+                {
+                    sums.AddRange(GetAllSumsOfSubTrianglesFromLocation(jaggedArray, i, j));
+                }
+            }
+
+            return sums.OrderBy(x => x).Take(amountOfSums).ToArray();
+        }
+
+        private IEnumerable<int> GetAllSumsOfSubTrianglesFromLocation(int[][] jaggedArray, int y, int x)
+        {
+            var width = 0;
+            var layers = new List<int>();
+            for (var i = y; i < jaggedArray.Length; i++)
+            {
+                var layerSum = 0;
+                width ++;
+                for (var j = x; j < x + width; j++)
+                {
+                    layerSum += jaggedArray[i][j];
+                }
+
+                layers.Add(layerSum);
+            }
+
+            "kjdsf".Split(' ');
+
+            return GetSumsFromLayers(layers);
+        }
+
+        private IEnumerable<int> GetSumsFromLayers(List<int> layers)
+        {
+            var sums = new List<int>();
+            var runningTotal = 0;
+
+            foreach (var layer in layers)
+            {
+                runningTotal += layer;
+                sums.Add(runningTotal);
+            }
+
+            return sums; 
         }
     }
 }
--- ProjectEulerTests/Problems/Problem150Tests.cs
+++ ProjectEulerTests/Problems/Problem150Tests.cs
@@ -5,6 +5,14 @@ namespace ProjectEulerTests.Problems
 {
     public class Problem150Tests
     {
+        private Problem150 _instance;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _instance = new Problem150();
+        }
+
         [Test]
         public void Single()
         {
@@ -15,7 +23,7 @@ namespace ProjectEulerTests.Problems
             jaggedArray[2] = new[] { 1, 1, 1 };
 
             //act
-            var actual = Problem150.GetSmallestSubtriangleSum(jaggedArray, 1);
+            var actual = _instance.GetSmallestSubtriangleSum(jaggedArray, 1);
 
             //assert
             Assert.That(actual[0], Is.EqualTo(-150));
@@ -31,7 +39,7 @@ namespace ProjectEulerTests.Problems
             jaggedArray[2] = new[] { 1, -1, -1 };
 
             //act
-            var actual = Problem150.GetSmallestSubtriangleSum(jaggedArray, 1);
+            var actual = _instance.GetSmallestSubtriangleSum(jaggedArray, 1);
 
             //assert
             Assert.That(actual[0], Is.EqualTo(-3));
@@ -47,7 +55,7 @@ namespace ProjectEulerTests.Problems
             jaggedArray[2] = new[] { -1, -1, -1 };
 
             //act
-            var actual = Problem150.GetSmallestSubtriangleSum(jaggedArray, 1);
+            var actual = _instance.GetSmallestSubtriangleSum(jaggedArray, 1);
 
             //assert
             Assert.That(actual[0], Is.EqualTo(-6));
@@ -57,42 +65,42 @@ namespace ProjectEulerTests.Problems
         public void Multiple()
         {
             //arrange
-            var jaggedArray = new int[3][];
+            var jaggedArray = new int[4][];
             jaggedArray[0] = new[] { -1 };
             jaggedArray[1] = new[] { -1, -1 };
             jaggedArray[2] = new[] { -1, -1, -1 };
-            jaggedArray[2] = new[] { -1, -1, -1, -1 };
+            jaggedArray[3] = new[] { -1, -1, -1, -1 };
 
             //act
-            var actual = Problem150.GetSmallestSubtriangleSum(jaggedArray, 3);
+            var actual = _instance.GetSmallestSubtriangleSum(jaggedArray, 3);
 
             //assert
             Assert.That(actual[0], Is.EqualTo(-10));
-            Assert.That(actual[0], Is.EqualTo(-6));
-            Assert.That(actual[0], Is.EqualTo(-6));
+            Assert.That(actual[1], Is.EqualTo(-6));
+            Assert.That(actual[2], Is.EqualTo(-6));
         }
 
         [Test]
         public void Example()
         {
             //arrange
-            var jaggedArray = new int[3][];
+            var jaggedArray = new int[6][];
             jaggedArray[0] = new[] { 15 };
             jaggedArray[1] = new[] { -14, -7 };
             jaggedArray[2] = new[] { 20, -13, -5 };
-            jaggedArray[2] = new[] { -3, 8, 23, -26 };
-            jaggedArray[2] = new[] { 1, -4, -5, -18, 5 };
-            jaggedArray[2] = new[] { -16, 31, 2, 9, 28, 3 };
+            jaggedArray[3] = new[] { -3, 8, 23, -26 };
+            jaggedArray[4] = new[] { 1, -4, -5, -18, 5 };
+            jaggedArray[5] = new[] { -16, 31, 2, 9, 28, 3 };
 
             //act
-            var actual = Problem150.GetSmallestSubtriangleSum(jaggedArray, 5);
+            var actual = _instance.GetSmallestSubtriangleSum(jaggedArray, 5);
 
             //assert
             Assert.That(actual[0], Is.EqualTo(-42));
-            Assert.That(actual[0], Is.EqualTo(-39));
-            Assert.That(actual[0], Is.EqualTo(-26));
-            Assert.That(actual[0], Is.EqualTo(-26 ));
-            Assert.That(actual[0], Is.EqualTo(-25));
+            Assert.That(actual[1], Is.EqualTo(-39));
+            Assert.That(actual[2], Is.EqualTo(-26));
+            Assert.That(actual[3], Is.EqualTo(-26 ));
+            Assert.That(actual[4], Is.EqualTo(-25));
         }
     }
 }
